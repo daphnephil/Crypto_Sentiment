cebcf4cc518e662e857cb2a84fcea89c1022c687
daphnephil <62921301+daphnephil@users.noreply.github.com>
2024-04-02 21:31:32 +0200

    Update variables.tf

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -17,7 +17,7 @@ variable "container_memory" {
 variable "project_id" {
   type        = string
   description = "The name of the project"
-  default     = "unique-gcp-project-id"
+  default     = "dtc-de-course-380621"
 }
 
 variable "region" {
