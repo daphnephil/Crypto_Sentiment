9c89dac50e9d3ba7c163b7d3be6956a5b83fecb7
daphnephil <62921301+daphnephil@users.noreply.github.com>
2024-04-17 19:03:24 +0200

    Add files via upload

---
+++ gcp/crypto_sentiment_pipeline/data_loaders/load_crypto_data_from_api.py
@@ -0,0 +1,29 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'https://www.cryptodatadownload.com/cdd/Bitstamp_BTCUSD_d.csv'
+    
+    header_index = 1
+    # Load the data from the CSV file
+    df = pd.read_csv(url, header=header_index)
+
+    return df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
